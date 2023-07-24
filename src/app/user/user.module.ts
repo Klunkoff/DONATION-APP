eea80a2d1ef744556e3a950910cdd9515816fe1f
Dimitar Klunkoff <klunkoff@gmail.com>
2023-07-24 18:32:04 +0300

    Creating Register and Profile components, add routing

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { UserRoutingModule } from './user-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
