4fc57d820b2c6e7b8ec6e8441c3bf23663d5769f
Dimitar Klunkoff <111648794+Klunkoff@users.noreply.github.com>
2023-07-28 15:49:47 +0300

    Creating new interface DonationPost

---
+++ src/app/types/donationPost.ts
@@ -0,0 +1,9 @@
+export interface DonationPost {
+
+    postTitle: string;
+    description: string;
+    category: string;
+    contact: string;
+    photo: string;
+    userIUD: string | undefined;
+}
