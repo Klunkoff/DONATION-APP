e97da33d8ad3357eca68705cf470d992ff97b13a
Dimitar Klunkoff <klunkoff@gmail.com>
2023-08-01 23:25:55 +0300

    Creating Find User By ID logic

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getAuth, signInWithEmailAndPassword, User, signOut, createUserWithEmailAndPassword } from '@firebase/auth';
-
+import { doc, getDoc } from 'firebase/firestore'; 
+import { db } from 'src/main';
 
 @Injectable({
   providedIn: 'root'
@@ -41,7 +42,6 @@ export class UserService {
       
       const userData = this.firestore.collection('users')
       .doc(userCredential.user.uid).set({ firstName: firstName, lastName: lastName, email: email });
-      console.log(userData);
 
     } catch (error) {
       const errorCode = (error as { code: string }).code;
@@ -57,6 +57,10 @@ export class UserService {
       const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
       this.user = userCredential.user;
       this.uid = this.user.uid;
+
+      this.findUserByUID(this.uid)
+      
+      
 
       this.router.navigate(['catalog']);
       
@@ -80,6 +84,18 @@ export class UserService {
       const errorMessage = (error as { message: string }).message;
       console.log(errorCode, errorMessage);
     }
+  }
+
+  async findUserByUID(uid: string) {
+
+    const docRef = doc(db, 'users', uid);
+    const docData = await getDoc(docRef);
+
+    const data = {data: docData.data()}
+    console.log(data);
+    
+    return docData.data();
+
   }
 
 
